b862ede111efb4c1d750922d5b634df5e8487346
FaisalAlharbi141 <fash.a.1818@gmail.com>
2025-09-30 11:48:13 +0300

    Finished TODO1

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,8 @@
+function StudentCard() {
+  return <div>
+    <h3>Name: Faisal Alharbi</h3>;
+    <p>ID: s202261000</p>
+    <p>Department: Information and Computer Secience</p>
+  </div>
+}
+export default StudentCard;
